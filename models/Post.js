a08028ed52001d0d219aa1ad82cc3e7f29141b65
Huong <huongnguyenak96@gmail.com>
2019-05-17 18:11:08 +0700

    createPost

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { UserModel } = require('./User');
+
 const PostSchema = new Schema({
     author: { type: Schema.Types.ObjectId, ref: 'user'},
     content: String,
@@ -11,4 +13,20 @@ const PostSchema = new Schema({
     ]
 })
 const PostModel = mongoose.model('post',PostSchema);
-module.exports = { PostModel }
+class Post{
+    static async createPost(author, content){
+        const post = await PostModel.create({author, content})
+        if(!post) throw new Error('Cannot create post!');
+        const user = await UserModel.findOneAndUpdate(
+            { _id: author},
+            { $addToSet: { posts: post._id} },
+            { new: true}
+        )
+        if(!user){
+            await PostModel.findByIdAndRemove(post._id)
+            throw new Error('Cannot update user!');
+        }
+        return post;
+    }
+}
+module.exports = { PostModel, Post }
