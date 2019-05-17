e4eadd798a4b565918181872e9207a36c86bb2c6
Huong <huongnguyenak96@gmail.com>
2019-05-17 18:21:28 +0700

    create post

--- controllers/post.router.js
+++ controllers/post.router.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const router = express.Router();
+const { Post } = require('../models/Post');
 
 // create
 router.post('/create',(req,res)=>{
     const author = req.userId;
     const content = req.body.content
-    
+    Post.createPost(author,content)
+    .then(post=>res.send({
+        success: true,
+        data: post,
+        message: ''
+    }))
+    .catch(err=>res.send({
+        success: false,
+        data: null,
+        message: err.message
+    }))
 })
 // update
 // delete
